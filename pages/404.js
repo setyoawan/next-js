efedfe7bdf76ad6e9a18d9d6923ef001a74ffda3
setyoawan <setyoawan8@gmail.com>
2022-04-08 15:09:47 +0700

    auto redirect page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,19 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const { default: Link } = require("next/link")
 
 const NotFound = () => {
+
+    // auto redirect 
+    const router = useRouter();
+
+    useEffect( () => {
+        setTimeout( () => {        
+        router.push('/')
+        }, 3000)
+    }, [])
+
     return(
         <div className="not-found">
             <h1>page not found</h1>
